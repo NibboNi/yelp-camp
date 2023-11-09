af422f211c49f8134d5d0d88e3efea0c50e4ac3e
Nicolás <131214845+NibboNi@users.noreply.github.com>
2023-11-09 10:38:44 -0600

    add images

--- seeds/index.js
+++ seeds/index.js
@@ -18,8 +18,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      price: price,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, cumque! Sequi unde id similique fugiat ratione minima corporis hic blanditiis, non est culpa at tempore. Asperiores, nihil consequatur. Deleniti blanditiis aut distinctio, necessitatibus veritatis corrupti ea aliquam facere optio beatae delectus, molestias nam quidem vero debitis consectetur numquam doloremque ducimus.",
       location: `${cities[random].city}, ${cities[random].state}`,
     });
     await camp.save();
